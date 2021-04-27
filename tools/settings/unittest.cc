26910a419b768b4be242b5e2d59bf831017d48a6
chunged4 <edchung482@gmail.com>
2021-04-26 17:35:46 -0700

    ver9

--- tools/settings/unittest.cc
+++ tools/settings/unittest.cc
@@ -536,7 +536,7 @@ void LaunchProjectile() {
   
   bool has_projectile = false;
   for (int i = 0; i < 100 && !has_projectile; i++) {
-    my_game.LaunchProjectile();
+    opponents.LaunchProjectile();
     if (my_game.GetOpponentProjectiles().size() > 0) {
       has_projectile = true;
     }
